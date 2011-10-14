ca7ea6e142c7d854efcd462ae290df28c436960e
Vincent Zoonekynd <zoonek@gmail.com>
2011-10-14 09:32:35 +0900

    Look for duplicated sizes before duplicated contents; do not read the whole file to compute the md5sum

--- find_double.pl
+++ find_double.pl
@@ -1,24 +1,52 @@
 #! perl -w
+
+# Look for duplicated files:
+# - First check the size of all files
+# - For sizes that appear several times (hopefully, this will exclude very large files),
+#   compute their MD5 sum.
+# - Print duplicated files
+
 use strict;
 use File::Find;
 use Digest::MD5 qw(md5);
 undef $/;
 $|++;
+
+our %s;
 our %f;
-sub wanted {
+
+sub size {
   stat($_);
   return unless -f _;
-  open(A, '<', $_) || return;
-  my $md5 = md5(<A>);
-  close A;
-  #  print STDERR "$md5 $_ $File::Find::name\n";
+  my $size = -s _;
+  #print STDERR "SIZE: $size $_ $File::Find::name\n";
+  $s{$size} = [] unless exists $s{$size};
+  push @{ $s{$size}  }, $File::Find::name;
+}
+
+sub contents {
+  stat($_);
+  return unless -f _;
+  my $size = -s _;
+  return unless scalar @{ $s{$size} } > 1;
+
+  open A, "-|", "md5sum", "-b", "--", $_;  # Do not read the whole file in Perl: it can be large.
+  chomp( my $md5 = <A> );
+  $md5 =~ s/ .*//gsm;
+  close(A);
+
+  #print STDERR "MD5: $md5 *** $_ *** $File::Find::name\n";
   $f{$md5} = [] unless exists $f{$md5};
   push @{ $f{$md5}  }, $File::Find::name;
 }
-find(\&wanted, '.');
+
+find(\&size,     '.');
+find(\&contents, '.');
+
 foreach my $m (keys %f) {
   if( scalar @{ $f{$m} } > 1 ) {
-    print join("\n", @{ $f{$m} });
+    my $size = -s $f{$m}->[0];
+    print $size, " ", join("\n" . $size . " ", @{ $f{$m} });
     print "\n\n";
   }
 }
